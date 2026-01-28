66b9b5c8049c1a12530d641bc50b579f86953186
nagajay33 <nagajay33@gmail.com>
2026-01-28 16:34:07 +0530

    Add initial cricketer data to insert_data.sql
    Insert multiple cricketer records into the database.

---
+++ data/sql/insert_data.sql
@@ -0,0 +1,14 @@
+INSERT INTO cricketers (cricketer_id, last_name, first_name, salary) VALUES
+(1, 'Kohli', 'Virat', 2500000.00),
+(2, 'Sharma', 'Rohit', 2300000.00),
+(3, 'Dhoni', 'MS', 3000000.00),
+(4, 'Williamson', 'Kane', 2100000.00),
+(5, 'Root', 'Joe', 2000000.00),
+(6, 'Smith', 'Steve', 2400000.00),
+(7, 'Babar', 'Azam', 2200000.00),
+(8, 'Warner', 'David', 2150000.00),
+(9, 'Cummins', 'Pat', 1900000.00),
+(10, 'Stokes', 'Ben', 2250000.00),
+(11, 'Kohli', 'Virat', 2600000.00),
+(12, 'Kohli', 'Virat', 2600000.00),
+(13, 'Kohli', 'Virat', 2600000.00);
